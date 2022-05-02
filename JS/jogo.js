9beba225ad3b5ce4d6a8c33bef62421dd2dcbc8a
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-05-02 11:58:46 -0300

    Declarações das funções

---
+++ JS/jogo.js
@@ -0,0 +1,33 @@
+function escolhaPersonagem(escolha) {
+    if (escolha === "1") {
+        return window.location.href = ".././HTML/dickVigarista/Fase1/situacao1.html";
+    }
+    else if (escolha === "2") {
+        return window.location.href = ".././HTML/penelopeCharmosa/Fase1/situacao1.html";
+    }
+    else {
+        return window.location.href = ".././HTML/professorAereo/Fase1/situacao1.html";
+    }
+}
+
+function validaEscolha(opcoes = []) {
+    do {
+        const escolha = prompt("Escolha sua opção: " + opcoes);
+
+        if (opcoes.indexOf(escolha) !== -1) {
+            return escolha;
+        }
+        alert("Escolha incorreta! Escolha:" + opcoes);
+
+    } while (true);
+}
+
+function decisao(escolha, link1, link2) {
+    if (escolha === "1") {
+        return window.location.href = link1;
+    }
+    else {
+        return window.location.href = link2;
+    }
+}
+
